1c8bfe6842cca7bd34a3c37acc25be3d33e1cf10
chengyue-lin <linchengyue0216@gmail.com>
2021-06-10 02:39:35 -0700

    Some update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CS172_final
+
+
+The crawler is done by Xianghu Wang. The elasticsearch part is done by Chengyue Lin.    
+And both of us work together to finish the extend part.   
+
+Part 1:    
+
+Part 2:   
+
+Part 3:   
--- elastic_search.py
+++ elastic_search.py
@@ -1,13 +1,45 @@
 from elasticsearch import Elasticsearch
-
+import os
+from elasticsearch import helpers
+import json
 es=Elasticsearch([{'host':'localhost','port':9200}])
-# es <Elasticsearch([{'host': 'localhost', 'port': 9200}])>
+
 es = Elasticsearch(timeout= 600, hosts = "http://localhost:9200/")
 #需创建index_mapping结构
-#index_mapping = {
+#是否要创建 setting 函数？
+index_mapping= {
+        "mappings": {
+                "document" : {
+                        "properties":{
+                                "title" :{
+                                        'type' : 'text'
+                                },
+                                "date" : {
+                                        'type' : 'text'
+                                },
+                                "keyword" : {
+                                        'type' : 'text'
+                                },
+                                "source" : {
+                                        'type' : 'text'
+                                },
+                                "link" : {
+                                        'type' : 'text'
+                                }
+                        }
+                }
+        }
+}
 
-#}
-print(es.ping())
+data =open(".json")   # input json file name
+i =1
+for q in data:   # input json file into the elastic search
+        content = data.read()
+        es.index(index='myindex', ignore=400, doc_type='doc', id=i, body=json.loads(content))
+        i= i+1
+
+
+#es.indices.create(index='first_one', ignore= 400)
 
 #if es.indices.exists() is not True:
 #    es.indices.create()
